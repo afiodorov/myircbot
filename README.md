aa84a41de2daddcde2c8cd227c98647f0c961140
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-09-22 17:28:05 +0100

    essentials added

---
+++ README.md
@@ -0,0 +1,19 @@
+## Installation
+
+Create config like
+
+~~~
+module.export = {
+  channels: ['channel1']
+  server: 'irc.gitter.im',
+  botName: YOURNAME,
+  password: YOURTOKEN
+};
+~~~
+
+
+## Running
+
+~~~.shell
+node run.js
+~~~
---
+++ run.js
@@ -0,0 +1,31 @@
+var config = require('./config.js');
+var irc = require('irc');
+
+var bot = new irc.Client(config.server, config.botName, {
+      userName: config.botName,
+      realName: config.botName,
+      password: config.password,
+      port: 6697,
+      localAddress: null,
+      debug: false,
+      showErrors: false,
+      autoRejoin: true,
+      autoConnect: true,
+      channels: config.channels,
+      secure: true,
+      selfSigned: true,
+      certExpired: true,
+      floodProtection: false,
+      floodProtectionDelay: 1000,
+      sasl: true,
+      stripColors: false,
+      channelPrefixes: '&#',
+      messageSplit: 512,
+      encoding: 'UTF-8'
+    }
+);
+
+bot.addListener('registered', function(message) {
+  console.log(message);
+  bot.say('afiodorov', 'First bot ever');
+});
